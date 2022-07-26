cc7685a271eb74ca87ea8ed9987fd01f485adb1e
MarinaLebl <marinabplebl@gmail.com>
2022-07-26 13:08:51 -0300

    inicio

---
+++ academia python/main.py
@@ -0,0 +1,10 @@
+import funcoes
+nome=input("Qual o seu nome? ")
+print(f"Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!")                           
+print("As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"! ")
+input("Aperte ENTER para continuar...")
+print("O jogo já vai começar! Lá vem a primeira questão!  ")
+print("Vamos começar com questões do nível FACIL!")                                      
+input("Aperte ENTER para continuar...")
+id=0
+nivel="facil"

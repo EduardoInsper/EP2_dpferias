6de117e6ae4d8780a85d266b3bbac0fa0f415cd4
MarinaLebl <marinabplebl@gmail.com>
2022-07-26 13:41:46 -0300

    opcoes da questao

--- academia python/main.py
+++ academia python/main.py
@@ -12,16 +12,17 @@ id=0
 nivel="facil"
 lista_sorteadas=list()
 lista_dinheiro=[0, 1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
-dinheiro=lista_dinheiro[0]
-while continua!="exit" or continua!="EXIT":
+while continua!="exit" or continua!="EXIT" or dinheiro==100000:
     questao= funcoes.sorteia_questao_inedida(dados.dados, nivel, lista_sorteadas)
     lista_sorteada.append(questao)
     print(funcao.questao_para_texto(questao, id))
     resposta= input(" ")
-    if resposta==questao["correta"]:
-        dinheiro+= lista_dinheiro[id]
-        print("Você acertou! Seu prêmio atual é de R$ {dinheiro:.2f} ")
-        continua=input("Aperte ENTER para continuar ou EXIT para sair... ")
-    else:
-        print("Que pena! Você errou e vai sair sem nada:(")
-        continua=input("Aperte EXIT para sair... ")
+    opcoes=[A, B, C, D, pula, ajuda]
+    if resposta in opcoes:
+        if resposta==questao["correta"]:
+            dinheiro= lista_dinheiro[id]
+            print("Você acertou! Seu prêmio atual é de R$ {dinheiro:.2f} ")
+            continua=input("Aperte ENTER para continuar ou EXIT para sair... ")
+        else:
+            print("Que pena! Você errou e vai sair sem nada:(")
+            continua=input("Aperte EXIT para sair... ")

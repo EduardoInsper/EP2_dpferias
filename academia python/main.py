78e5d5034d5bcaf82f3dda37f55f0a451200d214
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-07-27 09:04:35 -0300

    jogo

--- academia python/main.py
+++ academia python/main.py
@@ -1,20 +1,22 @@
 import funcoes
 import dados
+print('\nOlá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n')
 nome=input("Qual o seu nome? ")
-print(f"Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!")                           
-print("As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"! ")
+print(f"\nOk {nome}, você tem direito a pular 3 vezes e 2 ajudas!")                           
+print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n')
 input("Aperte ENTER para continuar...")
-print("O jogo já vai começar! Lá vem a primeira questão!  ")
+print("\nO jogo já vai começar! Lá vem a primeira questão!\n")
 print("Vamos começar com questões do nível FACIL!")                                      
 continua=input("Aperte ENTER para continuar...")
 pular=3
 ajudas=2
 id=1
 nivel="facil"
-lista_sorteada=list()
+lista_sorteada=[]
 lista_dinheiro=[0, 1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
+dinheiro = lista_dinheiro[id]
 while continua!="exit" and continua!="EXIT" and dinheiro!=100000:
-    questao= funcoes.sorteia_questao_inedida(dados.dados, nivel, lista_sorteadas)
+    questao= funcoes.sorteia_questao_inedida(dados.dados, nivel, lista_sorteada)
     lista_sorteada.append(questao)
     print(funcoes.questao_para_texto(questao, id))
     resposta= input(" ")
@@ -22,7 +24,7 @@ while continua!="exit" and continua!="EXIT" and dinheiro!=100000:
     if resposta in opcoes:
         id+=1
         if resposta==questao["correta"]:
-            dinheiro= lista_dinheiro[id]
+            dinheiro = lista_dinheiro[id]
             print("Você acertou! Seu prêmio atual é de R$ {dinheiro:.2f} ")
             continua=input("Aperte ENTER para continuar ou EXIT para sair... ")
         else:

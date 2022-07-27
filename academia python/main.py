82a5db9b1a53f4bca3955ecf075afc64501e299c
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-07-27 09:45:38 -0300

    Faltam as ajudas

--- academia python/main.py
+++ academia python/main.py
@@ -22,14 +22,47 @@ while continua!="exit" and continua!="EXIT":
     questao = funcoes.sorteia_questao_inedida(dados.dados, nivel, lista_sorteada)
     lista_sorteada.append(questao)
     print(funcoes.questao_para_texto(questao, id))
-    resposta= input(" ")
+    resposta= input("\nSua resposta: ")
     opcoes=["A", "B", "C", "D", "pula", "ajuda"]
-    if resposta in opcoes:
+    while resposta not in opcoes:
+        print('Opção inválida!')
+        print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
+        resposta= input("\nSua resposta: ")
+    else:
         if resposta==questao["correta"]:
             dinheiro = lista_dinheiro[id]
             print(f"Você acertou! Seu prêmio atual é de R$ {dinheiro} ")
-            continua=input("Aperte ENTER para continuar ou EXIT para sair... ")
+            continua=input("Aperte ENTER para continuar ou EXIT para sair...\n\n")
             id+=1
+        elif resposta=="pula":
+            if pular>0:
+                pular -= 1
+                if pular==0:
+                    print(f"Ok, pulando! ATENÇÃO: Você não tem mais direito a pulos!")
+                else:
+                    print(f"Ok, pulando! Você ainda tem {pular} pulos! ")
+                continua=input("Aperte ENTER para continuar...\n\n")
+            else:
+                print("Não deu! Você não tem mais direito a pulos!")
+                continua=input("Aperte ENTER para continuar...\n\n")
+                print(funcoes.questao_para_texto(questao, id))
+                resposta= input("\nSua resposta: ")
+                if resposta==questao["correta"]:
+                    dinheiro = lista_dinheiro[id]
+                    print(f"Você acertou! Seu prêmio atual é de R$ {dinheiro} ")
+                    continua=input("Aperte ENTER para continuar ou EXIT para sair...\n\n")
+                    id+=1
+                else:
+                    print("Que pena! Você errou e vai sair sem nada:(")
+                    continua=input("Aperte EXIT para sair ou ENTER para recomeçar... ")
+                    if continua=="exit" or continua=="EXIT":
+                        print("Obrigado por jogar!")
+                        break
+                    else:
+                        print("O jogo vai recomeçar!")
+                        continua=input("Aperte ENTER para continuar... ")
+                        id=1
+                        lista_sorteada=[]
         else:
             print("Que pena! Você errou e vai sair sem nada:(")
             continua=input("Aperte EXIT para sair ou ENTER para recomeçar... ")
